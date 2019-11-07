d42b03c0f5e6934f7652baad6052804326fc6ff1
zhuziyu0727 <55662983+zhuziyu0727@users.noreply.github.com>
2019-11-07 15:17:55 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,38 @@ import java.text.DecimalFormat;
 /**
  * This contains the main function to trigger the project.
  */
-public class Main implements TransactionMessage{
-    public static void main(String[] args) {
-        Bank bank = Bank.getInstance();
+public class Main implements DataOperation {
+	public static void main(String[] args) {
+		try {
+			DataOperation.sqliteRead();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Bank bank = Bank.getInstance();
 
-       bank.setManager("admin", "Admin000", "admin@gmail.com");
+		bank.setManager("admin", "Admin000", "admin@gmail.com");
 
-       bank.registerNewCustomer("dezhouw", "Aa123456", "dezhouw@sina.com");
+		//bank.registerNewCustomer("dezhouw", "Aa123456", "dezhouw@sina.com");
 
+		GUIBankATMFrame tf = new GUIBankATMFrame();
+		tf.showLogin();
 
-        GUIBankATMFrame tf = new GUIBankATMFrame();
-        tf.showLogin();
+		CurrencyExchangeRate rates = CurrencyExchangeRate.getInstance();
+		rates.setRate("USD", "RMB", 5);
+		rates.setRate("USD", "CCC", 8);
+		// System.out.println(rates.getRate("USD", "RMB"));
 
-        CurrencyExchangeRate rates = CurrencyExchangeRate.getInstance();
-        rates.setRate("USD", "RMB", 5);
-        rates.setRate("USD", "CCC", 8);
-        System.out.println(rates.getRate("USD", "RMB"));
-        
-        tf.addWindowListener(new   java.awt.event.WindowAdapter()   { 
-        	public   void   windowClosing(java.awt.event.WindowEvent   e)   { 
-        		 try {
-        				TransactionMessage.writeFile();
-        			} catch (IOException e2) {
-        				// TODO Auto-generated catch block
-        				e2.printStackTrace();
-        			}
-        	} 
-        	});
-        
-       
-    }
+		tf.addWindowListener(new java.awt.event.WindowAdapter() {
+			public void windowClosing(java.awt.event.WindowEvent e) {
+				try {
+					DataOperation.sqliteWrite();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+
+	}
 }
